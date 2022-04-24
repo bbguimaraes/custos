7cd0069181fb148455dab921512736c066d0be5a
Bruno Barcarol Guimarães <bbguimaraes@bbguimaraes.com>
2022-04-24 09:36:10 +0000

    Add maximum update count argument

--- src/utils.h
+++ src/utils.h
@@ -3,9 +3,11 @@
 
 #include <errno.h>
 #include <stdarg.h>
+#include <stdbool.h>
 #include <stddef.h>
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
 #define ARRAY_SIZE(x) (sizeof(x) / sizeof(*x))
 #define LOG_ERR(...) log_err(__FILE__, __LINE__, __func__, __VA_ARGS__)
@@ -23,6 +25,16 @@ static inline void vlog_err(
 static inline void vlog_errno(
     const char *restrict file, int line, const char *restrict fn,
     const char *restrict fmt, va_list args);
+
+/**
+ * Reads an unsigned integer value from a command-line argument.
+ * \param name Argument name, displayed in error messages.
+ * \param max Maximum return value allowed.
+ * \param s The argument string.
+ * \param p Output value.
+ */
+static inline bool parse_ulong_arg(
+    const char *name, size_t max, const char *s, unsigned long *p);
 
 inline void log_err(
     const char *restrict file, int line, const char *restrict fn,
@@ -58,6 +70,28 @@ inline void vlog_errno(
 {
     vlog_err(file, line, fn, fmt, args);
     fprintf(stderr, ": %s\n", strerror(errno));
+}
+
+inline bool parse_ulong_arg(
+    const char *name, size_t max, const char *s, unsigned long *p
+) {
+    const char *se = NULL;
+    unsigned long u = 0;
+    u = strtoul(s, (char**)&se, 10);
+    if(!u && se != s) {
+        fprintf(
+            stderr, "%s: invalid unsigned integer argument \"%s\": %s\n",
+            __func__, name, s);
+        return false;
+    }
+    if(max < u) {
+        fprintf(
+            stderr, "%s: argument \"%s\" out of range: %lu < %lu\n",
+            __func__, name, max, u);
+        return false;
+    }
+    *p = u;
+    return true;
 }
 
 #endif
