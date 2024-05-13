19400e65c18cff09defb23758b700d6238a2d3cb
Bruno Barcarol Guimarães <bbguimaraes@bbguimaraes.com>
2024-05-13 12:29:11 +0200

    Add simple monotonic update counter

--- src/thermal.h
+++ src/thermal.h
@@ -2,12 +2,13 @@
 #define CUSTOS_THERMAL_H
 
 #include <stdbool.h>
+#include <stddef.h>
 
 struct lua_State;
 
 void thermal_lua(struct lua_State *L);
 void *thermal_init(struct lua_State *L);
 bool thermal_destroy(void *d);
-bool thermal_update(void *d);
+bool thermal_update(void *d, size_t counter);
 
 #endif
